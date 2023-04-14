208e906fec713651e3ab6f5807b8c57ee069d5c6
mamaruf04 <mamar@DESKTOP-AR6EM10.(none)>
2023-04-14 20:14:20 +0600

    final

--- src/Component/Header/Header.js
+++ src/Component/Header/Header.js
@@ -1,12 +1,11 @@
 import React from "react";
-import image from '../../html/img/lws-logo.svg';
 
 const Header = () => {
   return (
     <div>
       <header id="header">
         <div className="container">
-          <img src={image} alt="logo" className="logo" />
+          <img src={'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQT8D2qKm3ZcE-NGL5We7bhbPG8EomMVnkRAA&usqp=CAU'} alt="logo" className="logo" />
           <div className="flex items-center">
             <a className="text-white min-w-[50px] font-medium" href="#">
               Home
